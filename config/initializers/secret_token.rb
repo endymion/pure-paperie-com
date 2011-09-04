50b3a6095477d80f3b53f02bedbc739c7494fc08
Ryan Alyn Porter <rap@endymion.com>
2011-09-04 14:05:48 -0400

    Un-carved block, generated by Rails 3.1.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PurePaperieCom::Application.config.secret_token = 'd9f07eec8b3bc1cd38b1ebced8bf4d9d3652cdebb252b1e7b51172f987256c9a25866469e811e61391e92e37c25633b4fe75ba8b148e6d3aa8cb2c40a86b62e3'
