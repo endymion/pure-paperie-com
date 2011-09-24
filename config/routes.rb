bdac990f9b846459a780e15d75ea74cd6be0d39f
Ryan Alyn Porter <rap@endymion.com>
2011-09-24 15:26:07 -0400

    Installed Active Admin, added Orders resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 PurePaperieCom::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   devise_for :users
 
   get "root/index"
--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,24 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110910185042) do
+ActiveRecord::Schema.define(:version => 20110924191959) do
 
-  create_table "categories", :force => true do |t|
-    t.string   "title"
+  create_table "active_admin_comments", :force => true do |t|
+    t.integer  "resource_id",   :null => false
+    t.string   "resource_type", :null => false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "namespace"
   end
 
-  create_table "orders", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.binary   "uuid",       :limit => 36
-  end
+  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
 
-  create_table "users", :force => true do |t|
+  create_table "admin_users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
     t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
@@ -36,11 +39,41 @@ ActiveRecord::Schema.define(:version => 20110910185042) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
+  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "categories", :force => true do |t|
+    t.string    "title"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+  end
+
+  create_table "orders", :force => true do |t|
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.binary    "uuid"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string    "email",                                 :default => "", :null => false
+    t.string    "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string    "reset_password_token"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
+    t.integer   "sign_in_count",                         :default => 0
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.string    "confirmation_token"
+    t.timestamp "confirmed_at"
+    t.timestamp "confirmation_sent_at"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
